42bfd2263d69e1b2fc02d022a2ee764e57938319
RybakovLG <lemixer.ry@gmail.com>
2023-07-06 11:23:33 +0300

    Updates

---
+++ static/js/20.4d56a201.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[20],{1026:function(a,s,d){}}]);
+//# sourceMappingURL=20.4d56a201.chunk.js.map
